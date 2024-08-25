2f76a049e6be7e09bb81ae8a749b976fb45dcb5a
Sushmithajsh7 <39213181+Sushmithajsh7@users.noreply.github.com>
2024-08-25 18:46:52 +0530

    Added functionality to handle new lines between numbers (instead of commas)

--- src/calculator.py
+++ src/calculator.py
@@ -1,10 +1,13 @@
+import re
+
+
 def add(numbers: str) -> int:
     sum = 0
 
     if numbers == "":
         return sum
 
-    number_list = map(int, numbers.split(","))
+    number_list = map(int, re.split(r',|\n', numbers))
 
     for number in number_list:
         sum += number
--- test/calculatorTests.py
+++ test/calculatorTests.py
@@ -11,3 +11,7 @@ class testStringCalculator(unittest.TestCase):
     # test case to take any number of inputs with comma as a delimiter
     def test_inputNumber(self):
         self.assertEqual(add("1,2,3"), 6)
+
+    # test case to consider new line character between numbers instead of comma
+    def test_newLine(self):
+        self.assertEqual(add("1\n2,3"), 6)
